9a04678608f25a8abe17ce29f2042f858a3e5426
gcollins1317 <gracecollins1000@gmail.com>
2018-07-13 17:52:05 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,8 @@
 class Transfer
-  # code here
+  attr_accessor :sender, :receiver, :amount 
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver 
+    @amount = amount 
+  end 
 end
